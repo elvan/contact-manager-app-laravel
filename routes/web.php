9954775b0b86d1cc736113052486be71e7bd5fef
Elvan Hidayat <elvanhd@gmail.com>
2023-03-02 22:48:22 +0700

    create basic templates and routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', function () {
+    return view('contacts.index');
+})->name('contacts.index');
+
+Route::get('/contacts/create', function () {
+    return "<h1>Add new contact</h1>";
+})->name('contacts.create');
+
+Route::get('/contacts/{id}', function ($id) {
+    return "Contact " . $id;
+})->name('contacts.show');
