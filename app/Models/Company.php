3b37808d53c361f5f474b6d1f66846f6033469f6
Elvan Hidayat <elvanhd@gmail.com>
2023-06-30 16:54:14 +0700

    implementing eloquent relationships

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Contact;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,9 @@ class Company extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'email', 'address', 'website'];
+
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class);
+    }
 }
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,9 @@ class Contact extends Model
     use HasFactory;
 
     protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'address'];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
