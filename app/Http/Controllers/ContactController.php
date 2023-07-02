bfb690d44c0a169d4c2d6425c2e729a13acc25f1
Elvan Hidayat <elvanhd@gmail.com>
2023-07-03 04:47:58 +0700

    sorting columns

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,7 +18,7 @@ class ContactController extends Controller
         $companies = $this->company->pluck();
 
         $contacts = Contact::allowedTrash()
-            ->allowedSorts('first_name')
+            ->allowedSorts(['first_name', 'last_name', 'email'], "-id")
             ->allowedFilters('company_id')
             ->allowedSearch('first_name', 'last_name', 'email')
             ->paginate(10);
