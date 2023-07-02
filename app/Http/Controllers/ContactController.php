0b38a4b55f33602a30a187cfde313cd8edc99d98
Elvan Hidayat <elvanhd@gmail.com>
2023-07-03 02:27:52 +0700

    deleting a contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -82,4 +82,12 @@ class ContactController extends Controller
 
         return redirect()->route('contacts.index')->with('message', 'Contact has been updated successfully');
     }
+
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+
+        return redirect()->route('contacts.index')->with('message', 'Contact has been removed successfully');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', WelcomeController::class);
 
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
-Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
+Route::resource('/contacts', ContactController::class);
 
 Route::resource('/companies', CompanyController::class);
