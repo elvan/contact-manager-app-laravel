5de226e2d75543cf2cda1f7a832551ba9e7e6d85
Elvan Hidayat <elvanhd@gmail.com>
2023-07-03 11:03:37 +0700

    implicit route-model-binding

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -50,25 +50,20 @@ class ContactController extends Controller
         return redirect()->route('contacts.index')->with('message', 'Contact has been added successfully');
     }
 
-    public function show($id)
+    public function show(Contact $contact)
     {
-        $contact = Contact::findOrFail($id);
-
         return view('contacts.show')->with('contact', $contact);
     }
 
-    public function edit($id)
+    public function edit(Contact $contact)
     {
         $companies = $this->company->pluck();
-        $contact = Contact::findOrFail($id);
 
         return view('contacts.edit', compact('companies', 'contact'));
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Contact $contact)
     {
-        $contact = Contact::findOrFail($id);
-
         $request->validate([
             'first_name' => 'required|string|max:50',
             'last_name' => 'required|string|max:50',
@@ -83,9 +78,8 @@ class ContactController extends Controller
         return redirect()->route('contacts.index')->with('message', 'Contact has been updated successfully');
     }
 
-    public function destroy($id)
+    public function destroy(Contact $contact)
     {
-        $contact = Contact::findOrFail($id);
         $contact->delete();
         $redirect = request()->query('redirect');
 
@@ -94,9 +88,8 @@ class ContactController extends Controller
             ->with('undoRoute', $this->getUndoRoute('contacts.restore', $contact));
     }
 
-    public function restore($id)
+    public function restore(Contact $contact)
     {
-        $contact = Contact::onlyTrashed()->findOrFail($id);
         $contact->restore();
 
         return back()
@@ -104,9 +97,8 @@ class ContactController extends Controller
             ->with('undoRoute', $this->getUndoRoute('contacts.destroy', $contact));
     }
 
-    public function forceDelete($id)
+    public function forceDelete(Contact $contact)
     {
-        $contact = Contact::onlyTrashed()->findOrFail($id);
         $contact->forceDelete();
 
         return back()
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ContactNoteController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +23,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', WelcomeController::class);
 
 Route::resource('/contacts', ContactController::class);
-Route::delete('/contacts/{contact}/restore', [ContactController::class, 'restore'])->name('contacts.restore');
-Route::delete('/contacts/{contact}/force-delete', [ContactController::class, 'forceDelete'])->name('contacts.force-delete');
+
+Route::delete('/contacts/{contact}/restore', [ContactController::class, 'restore'])
+    ->name('contacts.restore')
+    ->withTrashed();
+
+Route::delete('/contacts/{contact}/force-delete', [ContactController::class, 'forceDelete'])
+    ->name('contacts.force-delete')
+    ->withTrashed();
 
 Route::resource('/companies', CompanyController::class);
